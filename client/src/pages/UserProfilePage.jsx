19d4e623dbaf90f42e3aa77a9ef8d28a34478ce5
Varun Krishna <77630686+varunkrishna711@users.noreply.github.com>
2024-04-18 00:52:01 +0530

    add producer profile (#8)
    * minor ui fix
    
    * user profile page added
    
    * fix direction in leaflet map (#7)
    
    * fix bug in product request (#9)
    
    ---------
    
    Co-authored-by: Jaidan Sleeba <jaidansleeba22@gmail.com>
    Co-authored-by: Rijo Reji Mathew <99330953+rijomemuriyil@users.noreply.github.com>

--- client/src/pages/UserProfilePage.jsx
+++ client/src/pages/UserProfilePage.jsx
@@ -21,7 +21,6 @@ const UserProfilePage = () => {
       axios
         .get(`${process.env.REACT_APP_API_URL}/api/product/all/${id}`)
         .then((res) => {
-          console.log("==========================================", res.data);
           setProducts(res.data);
         });
     };
@@ -42,64 +41,6 @@ const UserProfilePage = () => {
   const handleClick = (id) => {
     navigate(`../../product/${id}`);
   };
-
-  //   return (
-  //     <div className="flex flex-col items-center min-h-[800px] content-center">
-  //       <div className="flex flex-col items-center content-center mt-8 producer-header">
-  //         <img
-  //           src={userData.photo ?? "https://via.placeholder.com/150"}
-  //           alt={userData.firstname}
-  //           className="object-cover w-32 h-32 m-0 rounded-full"
-  //         />
-  //         <p className="mt-4 text-2xl font-bold">
-  //           {userData.firstname} {userData.lastname}
-  //         </p>
-  //         <p className="mt-1 mb-1 text-green-700">#{userData._id}</p>
-  //         <p>{userData.email}</p>
-  //       </div>
-  //       <div className="flex flex-col items-center content-center w-full mt-8">
-  //         {userData.location && (
-  //           <MapContainer
-  //             center={userData.location}
-  //             zoom={15}
-  //             scrollWheelZoom={false}
-  //             style={{ height: "300px", width: "400px" }}
-  //           >
-  //             <TileLayer
-  //               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  //             />
-  //             <Marker position={userData.location}>
-  //               <Popup>{userData.name}</Popup>
-  //             </Marker>
-  //           </MapContainer>
-  //         )}
-  //       </div>
-  //       {products.length != 0 && (
-  //         <div className="w-full mt-8 producer-products">
-  //           <h3 className="mb-4 text-xl font-bold">Products</h3>
-  //           <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-  //             {console.log(products)}
-  //             {products?.map((product) => (
-  //               <li
-  //                 key={product.id}
-  //                 className="p-4 bg-white rounded-lg shadow"
-  //                 onClick={() => handleClick(product._id)}
-  //               >
-  //                 <img
-  //                   src={product.images[0]}
-  //                   alt={product.name}
-  //                   className="object-cover w-full h-32 mb-2"
-  //                 />
-  //                 <p className="font-bold">{product.name}</p>
-  //                 <p>{product.price}</p>
-  //               </li>
-  //             ))}
-  //           </ul>
-  //         </div>
-  //       )}
-  //     </div>
-  //   );
 
   return (
     <div className="flex flex-col items-center min-h-[800px] content-center">
@@ -133,29 +74,32 @@ const UserProfilePage = () => {
           </MapContainer>
         )}
       </div>
-      <div className="featuredproducts">
-        <div className="featuredproducts-content-wrapper">
-          <div className="mt-8 featuredproducts-maintext">Products</div>
-          <div className="flex items-center justify-evenly featuredproducts-cards">
-            {products.map((product) => (
-              <ProductCard
-                key={product._id}
-                product={product}
-                productId={product._id}
-                isBidding={product.isBidding}
-                productimage={product.images[0]}
-                name={product.name}
-                rating={product.rating}
-                reviewCount={product.reviews?.length}
-                categories={product.categories}
-                price={product.price}
-                sizes={product.sizes}
-                instock={product.instock}
-              />
-            ))}
+
+      {products.length != 0 && (
+        <div className="featuredproducts">
+          <div className="featuredproducts-content-wrapper">
+            <div className="mt-8 featuredproducts-maintext">Products</div>
+            <div className="flex items-center justify-evenly featuredproducts-cards">
+              {products.map((product) => (
+                <ProductCard
+                  key={product._id}
+                  product={product}
+                  productId={product._id}
+                  isBidding={product.isBidding}
+                  productimage={product.images[0]}
+                  name={product.name}
+                  rating={product.rating}
+                  reviewCount={product.reviews?.length}
+                  categories={product.categories}
+                  price={product.price}
+                  sizes={product.sizes}
+                  instock={product.instock}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
