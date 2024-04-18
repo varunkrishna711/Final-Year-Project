cabf77a23566d1f56ea624afd2429380a769dd11
jaidan22 <jaidansleeba22@gmail.com>
2024-04-18 08:54:25 +0530

    fix bugs

--- client/src/components/common/AccountMenu.jsx
+++ client/src/components/common/AccountMenu.jsx
@@ -1,49 +1,47 @@
-import React from 'react';
-import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-import { useDispatch } from 'react-redux';
-import { userLogout } from '../../store/userSlice';
+import React from "react";
+import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useDispatch } from "react-redux";
+import { userLogout } from "../../store/userSlice";
 
-const AccountMenu = ({content, setContent, closeAccountMenu}) => {
-
+const AccountMenu = ({ content, setContent, closeAccountMenu }) => {
   const dispatch = useDispatch();
 
   const setAccountDetailsContent = () => {
-    setContent('account')
+    setContent("account");
     if (closeAccountMenu) {
-      closeAccountMenu()
+      closeAccountMenu();
     }
-  }
+  };
   const setOrdersContent = () => {
-    setContent('orders')
+    setContent("orders");
     if (closeAccountMenu) {
-      closeAccountMenu()
+      closeAccountMenu();
     }
-  }
+  };
   const setAddressesContent = () => {
-    setContent('addresses')
+    setContent("addresses");
     if (closeAccountMenu) {
-      closeAccountMenu()
+      closeAccountMenu();
     }
-  }
+  };
   const handleLogout = () => {
-    dispatch(userLogout())
-  }
-
+    dispatch(userLogout());
+  };
 
   return (
     <div className="accountmenu">
       <ul className="menu">
-        <li 
-          className={content === 'account' ? "menu-item-active" : "menu-item"}
+        <li
+          className={content === "account" ? "menu-item-active" : "menu-item"}
           onClick={setAccountDetailsContent}
         >
           <div>Account details</div>
           <AccountCircleOutlinedIcon sx={{ fontSize: 20 }} />
         </li>
-        <li 
+        {/* <li 
           className={content === 'orders' ? "menu-item-active" : "menu-item"}
           onClick={setOrdersContent}
         >
@@ -56,17 +54,14 @@ const AccountMenu = ({content, setContent, closeAccountMenu}) => {
         >
           <div>Addresses</div>
           <HomeOutlinedIcon sx={{ fontSize: 21 }} />
-        </li>
-        <li 
-          className="menu-item menu-item-logout"
-          onClick={handleLogout}
-        >
+        </li> */}
+        <li className="menu-item menu-item-logout" onClick={handleLogout}>
           <div>Log out</div>
           <LogoutOutlinedIcon sx={{ fontSize: 20 }} />
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default AccountMenu;
